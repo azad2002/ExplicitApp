b3c5fc945aa0b3b4dd30b532d1d95a96caf5ddeb
azad2002 <azadfarooq397@gamail.com>
2024-04-02 01:35:03 +0530

    second commit

--- app/src/main/java/com/example/explicitapp/secondActivity.kt
+++ app/src/main/java/com/example/explicitapp/secondActivity.kt
@@ -7,5 +7,6 @@ class secondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        finish()
     }
 }
